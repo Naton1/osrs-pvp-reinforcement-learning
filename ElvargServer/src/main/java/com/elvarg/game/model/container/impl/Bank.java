fe9329c0b553f28bdf5532671fde2e69ac02aa41
Ynneh <007trey2k@gmail.com>
2022-09-06 18:26:40 +0100

    DespoitBox banking fix
    can't wrap the boolean without setting the status, but it's a deposit box so no need? just a boolean check for interface is fine.

--- ElvargServer/src/main/java/com/elvarg/game/model/container/impl/Bank.java
+++ ElvargServer/src/main/java/com/elvarg/game/model/container/impl/Bank.java
@@ -206,7 +206,7 @@ public class Bank extends ItemContainer {
      */
 
     public static void deposit(Player player, int item, int slot, int amount, boolean ignore) {
-        if (ignore || (player.getStatus() == PlayerStatus.BANKING && (player.getInterfaceId() == 5292 || player.getInterfaceId() == 4465))) {
+        if (ignore || player.getStatus() == PlayerStatus.BANKING && player.getInterfaceId() == 5292 || player.getInterfaceId() == 4465) {
             if (player.getInventory().getItems()[slot].getId() != item) {
                 return;
             }
