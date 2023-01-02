ece3a901a18301aadc00c2a62ff8c5f5e157df32
Toby Wisener <toby.wisener@commscope.com>
2023-01-02 12:06:19 +0000

    Tidying up and removing unused code

--- ElvargServer/src/main/java/com/elvarg/game/entity/impl/playerbot/commands/GoToDuelArena.java
+++ ElvargServer/src/main/java/com/elvarg/game/entity/impl/playerbot/commands/GoToDuelArena.java
@@ -1,29 +1,14 @@
 package com.elvarg.game.entity.impl.playerbot.commands;
 
-import com.elvarg.game.content.Dueling;
-import com.elvarg.game.content.minigames.impl.CastleWars;
 import com.elvarg.game.entity.impl.playerbot.PlayerBot;
-import com.elvarg.game.model.Animation;
-import com.elvarg.game.model.container.impl.Equipment;
 import com.elvarg.game.model.teleportation.TeleportHandler;
 import com.elvarg.game.model.teleportation.TeleportType;
 import com.elvarg.game.task.Task;
 import com.elvarg.game.task.TaskManager;
-import org.checkerframework.checker.units.qual.A;
-
-import static com.elvarg.game.GameConstants.GAME_ENGINE_PROCESSING_CYCLE_RATE;
 import static com.elvarg.game.entity.impl.playerbot.commands.CommandType.*;
 import static com.elvarg.game.model.teleportation.Teleportable.DUEL_ARENA;
-import static com.elvarg.util.Misc.getTicks;
 
 public class GoToDuelArena implements BotCommand {
-
-    private static final Animation WAVE_ANIM = new Animation(863);
-
-    /**
-     * 30 minutes in game ticks
-     */
-    private static final int THIRTY_MINUTES = getTicks(1800);
 
     @Override
     public String[] triggers() {
