129dba3a3d59823eeb159f42f4b6c28657ef16bc
syuil <120953055+syuil@users.noreply.github.com>
2023-01-06 16:52:16 -0500

    Cleaned up the code some more. Using TimedObjectReplacementTask to stay consistent with other classes

--- ElvargServer/src/main/java/com/elvarg/game/entity/impl/object/impl/WebHandler.java
+++ ElvargServer/src/main/java/com/elvarg/game/entity/impl/object/impl/WebHandler.java
@@ -4,16 +4,13 @@ import com.elvarg.game.Sound;
 import com.elvarg.game.Sounds;
 import com.elvarg.game.entity.impl.player.Player;
 import com.elvarg.game.entity.impl.object.GameObject;
-import com.elvarg.game.entity.impl.object.ObjectManager;
 import com.elvarg.game.model.Item;
 import com.elvarg.game.model.Animation;
-import com.elvarg.game.task.Task;
 import com.elvarg.game.task.TaskManager;
-import com.elvarg.game.model.Location;
+import com.elvarg.game.task.impl.TimedObjectReplacementTask;
 import com.elvarg.util.ItemIdentifiers;
 import com.elvarg.util.Misc;
 import com.elvarg.util.ObjectIdentifiers;
-
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -26,32 +23,18 @@ import java.util.regex.Pattern;
 
 public class WebHandler {
 
+    private static final int WEB_RESPAWN = 4000;
+    private static final Sound SLASH_SOUND = Sound.SLASH_WEB;
+    private static final Animation ITEM_ON_WEB_ANIMATION = new Animation(911);
+    private static final Pattern SHARP_ITEM_PATTERN = Pattern.compile("(.*2h.*|.*sword.*|.*dagger.*|.*rapier.*|.*scimitar.*|.*halberd.*|.*spear.*|.*axe.*|.*excalibur.*|.*claws.*|.*whip.*)", Pattern.CASE_INSENSITIVE);
     private static long lastSlash;
-    static Pattern sharpItemPattern = Pattern.compile("(.*2h.*|.*sword.*|.*dagger.*|.*rapier.*|.*scimitar.*|.*halberd.*|.*spear.*|.*axe.*|.*excalibur.*|.*claws.*|.*whip.*)", Pattern.CASE_INSENSITIVE);
-
 
     /**
-     * Handles the check when using an item on the web, ensuring it is a sharp item
+     * Handles if the inventory item used on web is sharp
      */
     public static boolean isSharpItem(Item item) {
-        Matcher sharpItemMatcher = sharpItemPattern.matcher(item.getDefinition().getName());
+        Matcher sharpItemMatcher = SHARP_ITEM_PATTERN.matcher(item.getDefinition().getName());
         return sharpItemMatcher.matches() || (item.getDefinition().getId() == ItemIdentifiers.KNIFE);
-    }
-
-    /**
-     * Handles if the player has the sharp item in the inventory
-     */
-    public static boolean hasSharpItem(Player player) {
-        for (Item item : player.getInventory().getItems()) {
-            if (item != null && item.getId() > 0 && item.getAmount() > 0) {
-                Matcher sharpItemMatcher = sharpItemPattern.matcher(item.getDefinition().getName());
-                if (sharpItemMatcher.matches()) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
     }
 
     /**
@@ -61,7 +44,7 @@ public class WebHandler {
     public static boolean wieldingSharpItem(Player player) {
         for (Item t : player.getEquipment().getItems()) {
             if (t != null && t.getId() > 0 && t.getAmount() > 0) {
-                Matcher sharpItemMatcher = sharpItemPattern.matcher(t.getDefinition().getName());
+                Matcher sharpItemMatcher = SHARP_ITEM_PATTERN.matcher(t.getDefinition().getName());
                 if (sharpItemMatcher.matches()) {
                     return true;
                 }
@@ -76,43 +59,24 @@ public class WebHandler {
      */
 
     public static void handleSlashWeb(Player player, GameObject web, boolean itemOnWeb) {
+        if (web == null) return;
 
-        if (web == null) {
-            return;
-        }
-        if (web.getDefinition().getName().equalsIgnoreCase("Web")) {
-            if (System.currentTimeMillis() - lastSlash < 4000) {
-                return;
-            }
-            if (wieldingSharpItem(player) || hasSharpItem(player)) {
-                if (itemOnWeb) {
-                    player.performAnimation(new Animation(911));
-                    Sounds.sendSound(player, Sound.SLASH_WEB);
-                } else {
-                    player.performAnimation(new Animation(player.getAttackAnim()));
-                    Sounds.sendSound(player, Sound.SLASH_WEB);
-                }
-                int i = Misc.random(2);
-                if (i < 2) {
-                    player.sendMessage("You slash the web apart.");
-                    ObjectManager.deregister(web, true);
-                    ObjectManager.register(new GameObject(ObjectIdentifiers.SLASHED_WEB, new Location(web.getLocation().getX(), web.getLocation().getY()), web.getType(), web.getFace(), null), true);
-                    TaskManager.submit(new Task(4000, false) {
+        if (!web.getDefinition().getName().equalsIgnoreCase("Web")) return;
 
-                        @Override
-                        public void execute() {
-                            ObjectManager.deregister(new GameObject(ObjectIdentifiers.SLASHED_WEB, new Location(web.getLocation().getX(), web.getLocation().getY()), web.getType(), web.getFace(), null), true);
-                            ObjectManager.register(new GameObject(ObjectIdentifiers.WEB, new Location(web.getLocation().getX(), web.getLocation().getY()), web.getType(), web.getFace(), null), true);
-                            stop();
-                        }
-                    });
-                } else {
-                    player.sendMessage("You fail to slash the web.");
-                }
-                lastSlash = System.currentTimeMillis();
-            } else {
-                player.sendMessage("Only a sharp blade can cut through this sticky web.");
-            }
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - lastSlash < 4000) return;
+
+        player.performAnimation(itemOnWeb ? ITEM_ON_WEB_ANIMATION : new Animation(player.getAttackAnim()));
+        Sounds.sendSound(player, SLASH_SOUND);
+
+        int successfulSlashChance = Misc.random(2);
+        if (successfulSlashChance < 2) {
+            player.sendMessage("You slash the web apart.");
+            TaskManager.submit(new TimedObjectReplacementTask(web, new GameObject(ObjectIdentifiers.SLASHED_WEB, web.getLocation(), web.getType(), web.getFace(), player.getPrivateArea()), WEB_RESPAWN));
+        } else {
+            player.sendMessage("You fail to slash the web.");
         }
+
+        lastSlash = currentTime;
     }
 }
