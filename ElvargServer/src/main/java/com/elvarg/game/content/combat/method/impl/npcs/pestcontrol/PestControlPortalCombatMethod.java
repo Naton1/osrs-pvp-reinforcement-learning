8c6c8482d182c48db9aee1e7207b7b16c2b20564
Ynneh <007trey2k@gmail.com>
2023-02-24 12:13:14 +0000

    - pest control npc class - pest control cobat ethod class

---
+++ ElvargServer/src/main/java/com/elvarg/game/content/combat/method/impl/npcs/pestcontrol/PestControlPortalCombatMethod.java
@@ -0,0 +1,41 @@
+package com.elvarg.game.content.combat.method.impl.npcs.pestcontrol;
+
+import com.elvarg.game.content.combat.CombatType;
+import com.elvarg.game.content.combat.hit.PendingHit;
+import com.elvarg.game.content.combat.method.CombatMethod;
+import com.elvarg.game.content.minigames.impl.pestcontrol.PestControl;
+import com.elvarg.game.entity.impl.Mobile;
+
+/**
+ * @author Ynneh | 24/02/2023 - 11:09
+ * <https://github.com/drhenny>
+ */
+public class PestControlPortalCombatMethod extends CombatMethod {
+
+    @Override
+    public CombatType type() {
+        return CombatType.MELEE;
+    }
+
+    @Override
+    public PendingHit[] hits(Mobile character, Mobile target) {
+        return new PendingHit[0];
+    }
+
+    @Override
+    public int attackDistance(Mobile character) {
+        return 5;
+    }
+
+    @Override
+    public boolean canAttack(Mobile character, Mobile target) {
+        return PestControl.isPortal(character.getAsNpc().getId(), false);
+    }
+
+    @Override
+    public void finished(Mobile character, Mobile target) {
+
+    }
+
+
+}
