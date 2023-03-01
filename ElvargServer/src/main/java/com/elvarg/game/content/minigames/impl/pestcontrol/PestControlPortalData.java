0e3e073b2c10ab28fd09e0afdad37c7f9c72be0d
Ynneh <007trey2k@gmail.com>
2023-03-01 14:05:18 +0000

    - Removed portal id from enum since it's - 4 to get the id

--- ElvargServer/src/main/java/com/elvarg/game/content/minigames/impl/pestcontrol/PestControlPortalData.java
+++ ElvargServer/src/main/java/com/elvarg/game/content/minigames/impl/pestcontrol/PestControlPortalData.java
@@ -12,11 +12,11 @@ import java.util.List;
  */
 public enum PestControlPortalData {
 
-    PURPLE("western", Direction.WEST, "a533ff", 1751, 1747, Arrays.asList(BonusManager.ATTACK_RANGE), 2628, 2591, 2631, 2592),
-    BLUE("eastern", Direction.EAST, "33d7ff", 1752, 1748, Arrays.asList(BonusManager.ATTACK_MAGIC), 2680, 2588, 2679, 2589),
-    YELLOW("south-eastern", Direction.SOUTH_EAST, "fff333", 1753, 1749, Arrays.asList(BonusManager.ATTACK_STAB, BonusManager.ATTACK_SLASH),
+    PURPLE("western", Direction.WEST, "a533ff", 1751, Arrays.asList(BonusManager.ATTACK_RANGE), 2628, 2591, 2631, 2592),
+    BLUE("eastern", Direction.EAST, "33d7ff", 1752, Arrays.asList(BonusManager.ATTACK_MAGIC), 2680, 2588, 2679, 2589),
+    YELLOW("south-eastern", Direction.SOUTH_EAST, "fff333", 1753, Arrays.asList(BonusManager.ATTACK_STAB, BonusManager.ATTACK_SLASH),
             2669, 2570, 2670, 2573),
-    RED("south-western", Direction.SOUTH_WEST, "e32a2a", 1754, 1750, Arrays.asList(BonusManager.ATTACK_CRUSH),2645, 2569, 2646, 2572)
+    RED("south-western", Direction.SOUTH_WEST, "e32a2a", 1754, Arrays.asList(BonusManager.ATTACK_CRUSH),2645, 2569, 2646, 2572)
     ;
 
     public Direction direction;
@@ -24,17 +24,16 @@ public enum PestControlPortalData {
     public String name, colourCode;
 
     public List<Integer> weaknesses;
-    public int shieldId, unshieldId;
+    public int shieldId;
 
     public int xPosition, yPosition, npcSpawnX, npcSpawnY;
 
-    PestControlPortalData(String name, Direction direction, String colourCode, int shieldId, int unshieldId, List<Integer> weaknesses, int xPosition, int yPosition, int npcSpawnX, int npcSpawnY) {
+    PestControlPortalData(String name, Direction direction, String colourCode, int shieldId, List<Integer> weaknesses, int xPosition, int yPosition, int npcSpawnX, int npcSpawnY) {
         this.name = name;
         this.direction = direction;
         this.colourCode = colourCode;
         this.weaknesses = weaknesses;
         this.shieldId = shieldId;
-        this.unshieldId = unshieldId;
         this.xPosition = xPosition;
         this.yPosition = yPosition;
         this.npcSpawnX = npcSpawnX;
