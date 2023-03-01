6ab0a186936379355e61a7537438260fdeb1d71f
Ynneh <007trey2k@gmail.com>
2023-03-01 09:34:32 +0000

    - Shifter NPC placeholder class

--- ElvargServer/src/main/java/com/elvarg/game/entity/impl/npc/impl/pestcontrol/ShifterNPC.java
+++ ElvargServer/src/main/java/com/elvarg/game/entity/impl/npc/impl/pestcontrol/ShifterNPC.java
@@ -1,23 +1,19 @@
 package com.elvarg.game.entity.impl.npc.impl.pestcontrol;
 
 import com.elvarg.game.content.combat.method.CombatMethod;
-import com.elvarg.game.content.combat.method.impl.npcs.CrazyArchaeologistCombatMethod;
-import com.elvarg.game.content.combat.method.impl.npcs.pestcontrol.PestControlPortalCombatMethod;
 import com.elvarg.game.content.combat.method.impl.npcs.pestcontrol.ShifterCombatMethod;
-import com.elvarg.game.content.combat.method.impl.npcs.pestcontrol.SplatterCombatMethod;
 import com.elvarg.game.entity.impl.npc.NPC;
 import com.elvarg.game.entity.impl.player.Player;
 import com.elvarg.game.model.Ids;
 import com.elvarg.game.model.Location;
 
 import static com.elvarg.util.NpcIdentifiers.*;
-import static com.elvarg.util.NpcIdentifiers.PORTAL_12;
 
 /**
  * @author Ynneh | 27/02/2023 - 07:52
  * <https://github.com/drhenny>
  */
-@Ids({1694, 1696, 1698, 1700, 1702})
+@Ids({SHIFTER, SHIFTER_3, SHIFTER_5, SHIFTER_7, SHIFTER_9})
 public class ShifterNPC extends NPC {
 
     private static final CombatMethod COMBAT_METHOD = new ShifterCombatMethod();
