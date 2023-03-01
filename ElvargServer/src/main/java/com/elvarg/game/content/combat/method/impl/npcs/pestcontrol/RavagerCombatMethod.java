30591ab06d00461a241787f0668d978309b3613c
Ynneh <007trey2k@gmail.com>
2023-03-01 09:31:55 +0000

    - Ravager NPC / CombatMethod placeholder

---
+++ ElvargServer/src/main/java/com/elvarg/game/content/combat/method/impl/npcs/pestcontrol/RavagerCombatMethod.java
@@ -0,0 +1,10 @@
+package com.elvarg.game.content.combat.method.impl.npcs.pestcontrol;
+
+import com.elvarg.game.content.combat.method.impl.MeleeCombatMethod;
+
+/**
+ * @author Ynneh | 01/03/2023 - 08:28
+ * <https://github.com/drhenny>
+ */
+public class RavagerCombatMethod extends MeleeCombatMethod {
+}
