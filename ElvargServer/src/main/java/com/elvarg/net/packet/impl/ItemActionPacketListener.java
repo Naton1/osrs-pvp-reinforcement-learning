1bfb3cbb3135c5cb51ff4d55c40fdd6d9625a972
Ynneh <007trey2k@gmail.com>
2023-08-31 19:23:37 +0100

    interaction classes for cannon

--- ElvargServer/src/main/java/com/elvarg/net/packet/impl/ItemActionPacketListener.java
+++ ElvargServer/src/main/java/com/elvarg/net/packet/impl/ItemActionPacketListener.java
@@ -55,6 +55,11 @@ public class ItemActionPacketListener implements PacketExecutor {
 			return;
 		}
 
+		if (itemId == ItemIdentifiers.CANNON_BASE) {
+			player.getDwarfCannon().setup();
+			return;
+		}
+
 		// Prayer
 		if (Prayer.buryBone(player, itemId)) {
 			return;
